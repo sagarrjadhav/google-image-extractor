95f988c699f5e06351d25f29ad8a1904504669aa
Sagar J <sajadhav.corp@gmail.com>
2018-02-13 14:47:23 -0500

    adjusted setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
     ],

6ff34f09904adbeb8c586162e9a4ef0dc908045b
Sagar J <sajadhav.corp@gmail.com>
2018-02-14 20:08:46 -0500

    fixed minor bugs and comments

--- example.py
+++ example.py
@@ -2,5 +2,5 @@ from giextractor import GoogleImageExtractor
 
 if __name__ == '__main__':
     imageExtractor = GoogleImageExtractor(
-        imageQuery='mango fruit', imageCount=200)
+        imageQuery='apple fruit', imageCount=200)
     imageExtractor.extract_images()
